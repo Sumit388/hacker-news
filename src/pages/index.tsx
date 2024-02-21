913b5045716106b19b21056db07bb62d88ec286d
Sumit Mishra <sumit.mishra@uniacco.com>
2024-02-22 01:20:40 +0530

    Fix: rearreging file because build failed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,13 @@
 /* //* Packages Imports */
-import { useState, useRef } from "react";
 import { GetServerSideProps } from "next";
 import axios from "axios";
-import dynamic from "next/dynamic";
-import { ToastContainer, toast } from "react-toastify";
 
 /* //* Components Imports */
-import Pagination from "@Components/common/Pagination";
-import DataSection from "@Components/homepage/DataSection";
 import Layout from "@Components/layout";
+import HomePage from "@Components/homepage/HomePage";
 
 /* //* Utils Imports */
-import { getListingData, HOST_URL } from "@Utils/urls";
-
-/* //* Styles Imports */
-import Styles from "@Styles/Homepage.module.scss";
-
-/* //* Dynamic Imports */
-const HeadingSection = dynamic(
-  () => import("@Components/homepage/HeadingSection"),
-  {
-    ssr: false,
-  }
-);
+import { getListingData } from "@Utils/urls";
 
 const page = ({
   data,
@@ -30,89 +15,7 @@ const page = ({
 }: {
   data: ListingDataType;
   queryString: string;
-}) => {
-  const searchParams = useRef(new URLSearchParams(queryString));
-  const [listingData, setListingData] = useState<ListingDataType>(data);
-  const [loading, setLoading] = useState<boolean>(false);
-
-  const getData = async () => {
-    setLoading(true);
-    try {
-      const data = await axios.get(
-        getListingData(searchParams.current.toString())
-      );
-      setListingData(data?.data);
-    } catch (error: ErrorType) {
-      console.error(error);
-      toast.error(
-        error?.response?.data?.message ||
-          " Some error occurred. Please try again."
-      );
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const handleChange = (key: string, value?: string) => {
-    if (!value && !searchParams.current.has(key)) return;
-    if (!value && searchParams.current.has(key)) {
-      searchParams.current.delete(key);
-    } else if (value) {
-      searchParams.current.set(key, value);
-    }
-
-    // remove page=1 from query, and redirect to page 1 on any filter change
-    if ((key === "page" && value === "1") || key !== "page") {
-      searchParams.current.delete("page");
-    }
-
-    const currentUrl = searchParams?.current?.toString()
-      ? `${HOST_URL}?${searchParams.current}`
-      : HOST_URL;
-
-    getData();
-    window.history.pushState({}, "", currentUrl);
-  };
-
-  const handlePageChange = (event: MouseEvent | any) => {
-    event.preventDefault();
-    setTimeout(() => {
-      document.body.scrollTop = 0;
-      document.documentElement.scrollTop = 0;
-    }, 100);
-    const params = new URLSearchParams(event.target.search);
-    if (params.get("page") !== searchParams.current.get("page")) {
-      searchParams.current.delete("page");
-      handleChange("page", params.get("page") as string);
-    }
-  };
-
-  return (
-    <div className={Styles.homepageContainer}>
-      {" "}
-      <HeadingSection handleChange={handleChange} />
-      <DataSection loading={loading} listingData={listingData} />
-      {listingData.hits.length > 0 && (
-        <Pagination
-          pageSize={20}
-          totalCount={Math.min(20 * listingData?.nbPages, 500)}
-          onClick={handlePageChange}
-          currentPage={
-            searchParams.current.has("page")
-              ? Number(searchParams.current.get("page"))
-              : 1
-          }
-          url={(() => {
-            const params = new URLSearchParams(searchParams.current);
-            params.delete("page");
-            return "localhost:3000" + "?" + params + "&page=";
-          })()}
-        />
-      )}
-      <ToastContainer />
-    </div>
-  );
-};
+}) => <HomePage data={data} queryString={queryString} />;
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   const queryString = Object.keys(query)
